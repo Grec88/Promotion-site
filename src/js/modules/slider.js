c4f451dc12d73e632e2b7df5c3db36a723ce48d2
Aleksey Likhtinov <91214571+Grec88@users.noreply.github.com>
2023-03-16 20:59:28 +0300

    Update slider

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -29,9 +29,20 @@ export class Slider {
     
 
     render() {
+        this.hanson = document.querySelector('.hanson');
+        this.hanson.style.opacity = '0';
         this.buttons.forEach(button => {
-            button.addEventListener('click', () => {
-                this.plusSlides(1);
+            button.addEventListener('click', (e) => {
+                if (e.target.closest('.difference') != null) {
+                    this.plusSlides(1);
+                    const timerId = setInterval(() => { 
+                        this.hanson.style.opacity = '1';
+                        this.hanson.classList.add('animated', 'slideInUp');
+                 }, 3000);
+                    setTimeout(() => { clearInterval(timerId) }, 4000);
+                } else {
+                    this.plusSlides(1);
+                }
             });
         });
 
