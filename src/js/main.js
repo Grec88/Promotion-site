921c191e3dc820e74097393d449a5c977079739f
Aleksey Likhtinov <91214571+Grec88@users.noreply.github.com>
2023-03-18 18:18:29 +0300

    Video player

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,6 @@ window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', ".next", ".sidecontrol > a");
     slider.render();
 
+    const player = new VideoPlayer('.showup .play', '.overlay');
+    player.init();
 });
