2dd6218435b465374843b6f2d750d4e092d82d2a
Aleksey Likhtinov <91214571+Grec88@users.noreply.github.com>
2023-03-22 16:13:11 +0300

    Accordion

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,36 +1,42 @@
 export class Accordion {
     constructor(textButton, downloadButton) {
-        this.textButton = document.querySelector(textButton);
-        this.downloadButton = document.querySelector(downloadButton);
+        this.textButtons = document.querySelectorAll(textButton);
+        this.downloadButtons = document.querySelectorAll(downloadButton);
     }
 
     bindTriggers() {
-        this.textButton.addEventListener('click', (e) => {
-            const textElem = e.target.closest(".module__info-show").nextElementSibling;
-            if (textElem.style.display === "none" || textElem.style.display === "") {
-                textElem.style.display = "flex";
-            } else {
-                textElem.style.display = "none";
-            }
-        })
-
-        this.downloadButton.addEventListener('click', () => {
-            const url = "src/assets/sample.pdf";
-
-            fetch(url, {
-                method: 'GET',
+        if (this.textButtons && this.downloadButtons) {
+            this.textButtons.forEach(textButton => {
+                textButton.addEventListener('click', (e) => {
+                    const textElem = e.target.closest(".module__info-show").nextElementSibling;
+                    if (textElem.style.display === "none" || textElem.style.display === "") {
+                        textElem.style.display = "flex";
+                    } else {
+                        textElem.style.display = "none";
+                    }
+                })
             })
-            .then((response) => response.blob())
-            .then((blob) => {
-                const url = URL.createObjectURL(new Blob([blob]));
-                const link = document.createElement('a');
-                link.href = url;
-                link.setAttribute('download', 'file.pdf');
-                document.body.appendChild(link);
-                link.click();
-                link.remove();
+
+            this.downloadButtons.forEach(downloadButton => {
+                downloadButton.addEventListener('click', () => {
+                    const url = "src/assets/sample.pdf";
+
+                    fetch(url, {
+                        method: 'GET',
+                    })
+                        .then((response) => response.blob())
+                        .then((blob) => {
+                            const url = URL.createObjectURL(new Blob([blob]));
+                            const link = document.createElement('a');
+                            link.href = url;
+                            link.setAttribute('download', 'file.pdf');
+                            document.body.appendChild(link);
+                            link.click();
+                            link.remove();
+                        })
+                })
             })
-        })
+        }
     }
 
     init() {
